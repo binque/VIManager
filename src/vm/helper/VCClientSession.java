d7426706061cbc0dd638b143485f834bef66188f
WaterMaster <shuifuzi@gmail.com>
2017-03-14 23:58:14 +0800

    完成了克隆虚拟机

---
+++ src/vm/helper/VCClientSession.java
@@ -0,0 +1,310 @@
+package vm.helper;
+
+/*
+ * Created by huxia on 2017/3/13.
+ */
+
+import com.vmware.vim25.*;
+import org.w3c.dom.Element;
+
+import javax.net.ssl.*;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.soap.SOAPFaultException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @功能描述：客户程序的登陆、登出和认证
+ */
+public class VCClientSession {
+    private static final String url = "https://10.251.0.20:8000/sdk";
+    private static final String username = "administrator@vsphere.local";
+    private static final String password = "@Vmware123";
+
+    private static final ManagedObjectReference SVC_INST_REF = new ManagedObjectReference();
+    private static VimService vimService;
+
+    public static VimService getVimService() {
+        return vimService;
+    }
+
+    private static VimPortType vimPort;
+
+    public static VimPortType getVimPort() {
+        return vimPort;
+    }
+
+    private static ServiceContent serviceContent;
+
+    public static ServiceContent getServiceContent() {
+        return serviceContent;
+    }
+
+    private static final String SVC_INST_NAME = "ServiceInstance";
+    private static boolean isConnected = false;
+
+    public static boolean IsConnected() {
+        return isConnected;
+    }
+
+    private static ManagedObjectReference perfManager;
+
+    public static ManagedObjectReference getPerfManager() {
+        return perfManager;
+    }
+
+    private static ManagedObjectReference propCollectorRef;
+
+    public static ManagedObjectReference getPropCollectorRef() {
+        return propCollectorRef;
+    }
+
+    private static class TrustAllTrustManager implements javax.net.ssl.TrustManager, javax.net.ssl.X509TrustManager {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        public boolean isServerTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public boolean isClientTrusted(java.security.cert.X509Certificate[] certs) {
+            return true;
+        }
+    }
+
+    private static void trustAllHttpsCertificates() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[1];
+        TrustManager tm = new TrustAllTrustManager();
+        trustAllCerts[0] = tm;
+        SSLContext sc = SSLContext.getInstance("SSL");
+        SSLSessionContext sslsc = sc.getServerSessionContext();
+        sslsc.setSessionTimeout(0);
+        sc.init(null, trustAllCerts, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+
+    /**
+     * @功能描述：连接认证
+     */
+    public static void Connect() throws RuntimeFaultFaultMsg, InvalidLoginFaultMsg, InvalidLocaleFaultMsg, KeyManagementException, NoSuchAlgorithmException {
+        HostnameVerifier hv = (s, sslSession) -> true;
+        trustAllHttpsCertificates();
+
+        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+
+        SVC_INST_REF.setType(SVC_INST_NAME);
+        SVC_INST_REF.setValue(SVC_INST_NAME);
+
+        vimService = new VimService();
+        vimPort = vimService.getVimPort();
+        Map<String, Object> ctxt = ((BindingProvider) vimPort).getRequestContext();
+
+        ctxt.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, url);
+        ctxt.put(BindingProvider.SESSION_MAINTAIN_PROPERTY, true);
+
+        serviceContent = vimPort.retrieveServiceContent(SVC_INST_REF);
+        vimPort.login(serviceContent.getSessionManager(), username, password, null);
+        isConnected = true;
+
+        perfManager = serviceContent.getPerfManager();
+        propCollectorRef = serviceContent.getPropertyCollector();
+
+        System.out.println(serviceContent.getAbout().getFullName());
+        System.out.println("Server type is " + serviceContent.getAbout().getApiType());
+    }
+
+    /**
+     * @功能描述：断开连接
+     */
+    public static void Disconnect() throws RuntimeFaultFaultMsg {
+        if (isConnected) {
+            vimPort.logout(serviceContent.getSessionManager());
+        }
+        isConnected = false;
+    }
+
+    /**
+     * @功能描述：打印错误信息
+     */
+    private static void printSoapFaultException(SOAPFaultException sfe) {
+        System.out.println("Soap fault: ");
+        if (sfe.getFault().hasDetail()) {
+            System.out.println(sfe.getFault().getDetail().getFirstChild().getLocalName());
+        }
+        if (sfe.getFault().getFaultString() != null) {
+            System.out.println("Message: " + sfe.getFault().getFaultString());
+        }
+    }
+
+    /**
+     * @param listpfs 属性过滤器集合
+     * @功能描述：根据属性检索要查询的对象信息
+     */
+    public static List<ObjectContent> RetrievePropertiesAllObjects(List<PropertyFilterSpec> listpfs) {
+        RetrieveOptions propObjectRetrieveOpts = new RetrieveOptions();
+        List<ObjectContent> listobjcontent = new ArrayList<>();
+        try {
+            RetrieveResult rslts = vimPort.retrievePropertiesEx(propCollectorRef, listpfs, propObjectRetrieveOpts);
+            if (rslts != null && rslts.getObjects() != null && !rslts.getObjects().isEmpty()) {
+                listobjcontent.addAll(rslts.getObjects());
+            }
+            String token = null;
+            if (rslts != null && rslts.getToken() != null) {
+                token = rslts.getToken();
+            }
+            while (token != null && !token.isEmpty()) {
+                rslts = vimPort.continueRetrievePropertiesEx(propCollectorRef, token);
+                token = null;
+                if (rslts != null) {
+                    token = rslts.getToken();
+                    if (rslts.getObjects() != null && !rslts.getObjects().isEmpty()) {
+                        listobjcontent.addAll(rslts.getObjects());
+                    }
+                }
+            }
+        } catch (SOAPFaultException sfe) {
+            printSoapFaultException(sfe);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listobjcontent;
+    }
+
+    public static Object[] WaitForValues(ManagedObjectReference objmor, String[] filterProps, String[] endWaitProps, Object[][] expectedVals) throws InvalidPropertyFaultMsg, RuntimeFaultFaultMsg, InvalidCollectorVersionFaultMsg {
+        ManagedObjectReference filterSpecRef = null;
+
+        // version string is initially null
+        String version = "";
+        Object[] endVals = new Object[endWaitProps.length];
+        Object[] filterVals = new Object[filterProps.length];
+        String stateVal = null;
+
+        PropertyFilterSpec spec = propertyFilterSpec(objmor, filterProps);
+
+        filterSpecRef = vimPort.createFilter(serviceContent.getPropertyCollector(), spec, true);
+
+        boolean reached = false;
+
+        UpdateSet updateset = null;
+        List<PropertyFilterUpdate> filtupary = null;
+        List<ObjectUpdate> objupary = null;
+        List<PropertyChange> propchgary = null;
+        while (!reached) {
+            updateset = vimPort.waitForUpdatesEx(serviceContent.getPropertyCollector(), version, new WaitOptions());
+            if (updateset == null || updateset.getFilterSet() == null) {
+                continue;
+            }
+            version = updateset.getVersion();
+
+            // Make this code more general purpose when PropCol changes later.
+            filtupary = updateset.getFilterSet();
+
+            for (PropertyFilterUpdate filtup : filtupary) {
+                objupary = filtup.getObjectSet();
+                for (ObjectUpdate objup : objupary) {
+                    // TODO: Handle all "kind"s of updates.
+                    if (objup.getKind() == ObjectUpdateKind.MODIFY
+                            || objup.getKind() == ObjectUpdateKind.ENTER
+                            || objup.getKind() == ObjectUpdateKind.LEAVE) {
+                        propchgary = objup.getChangeSet();
+                        for (PropertyChange propchg : propchgary) {
+                            updateValues(endWaitProps, endVals, propchg);
+                            updateValues(filterProps, filterVals, propchg);
+                        }
+                    }
+                }
+            }
+
+            Object expctdval = null;
+            // Check if the expected values have been reached and exit the loop
+            // if done.
+            // Also exit the WaitForUpdates loop if this is the case.
+            for (int chgi = 0; chgi < endVals.length && !reached; chgi++) {
+                for (int vali = 0; vali < expectedVals[chgi].length && !reached; vali++) {
+                    expctdval = expectedVals[chgi][vali];
+                    if (endVals[chgi] == null) {
+                        // Do Nothing
+                    } else if (endVals[chgi].toString().contains("val: null")) {
+                        // Due to some issue in JAX-WS De-serialization getting the information from
+                        // the nodes
+                        Element stateElement = (Element) endVals[chgi];
+                        if (stateElement != null && stateElement.getFirstChild() != null) {
+                            stateVal = stateElement.getFirstChild().getTextContent();
+                            reached = expctdval.toString().equalsIgnoreCase(stateVal) || reached;
+                        }
+                    } else {
+                        expctdval = expectedVals[chgi][vali];
+                        reached = expctdval.equals(endVals[chgi]) || reached;
+                        stateVal = "filtervals";
+                    }
+                }
+            }
+        }
+        Object[] retVal = null;
+        // Destroy the filter when we are done.
+        try {
+            vimPort.destroyPropertyFilter(filterSpecRef);
+        } catch (RuntimeFaultFaultMsg e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (stateVal != null) {
+            if (stateVal.equalsIgnoreCase("ready")) {
+                retVal = new Object[]{HttpNfcLeaseState.READY};
+            }
+            if (stateVal.equalsIgnoreCase("error")) {
+                retVal = new Object[]{HttpNfcLeaseState.ERROR};
+            }
+            if (stateVal.equals("filtervals")) {
+                retVal = filterVals;
+            }
+        } else {
+            retVal = new Object[]{HttpNfcLeaseState.ERROR};
+        }
+        return retVal;
+    }
+
+    private static PropertyFilterSpec propertyFilterSpec(ManagedObjectReference objmor, String[] filterProps) {
+        PropertyFilterSpec spec = new PropertyFilterSpec();
+        ObjectSpec oSpec = new ObjectSpec();
+        oSpec.setObj(objmor);
+        oSpec.setSkip(Boolean.FALSE);
+        spec.getObjectSet().add(oSpec);
+
+        PropertySpec pSpec = new PropertySpec();
+        pSpec.getPathSet().addAll(Arrays.asList(filterProps));
+        pSpec.setType(objmor.getType());
+        spec.getPropSet().add(pSpec);
+        return spec;
+    }
+
+    private static void updateValues(String[] props, Object[] vals, PropertyChange propchg) {
+        for (int findi = 0; findi < props.length; findi++) {
+            if (propchg.getName().lastIndexOf(props[findi]) >= 0) {
+                if (propchg.getOp() == PropertyChangeOp.REMOVE) {
+                    vals[findi] = "";
+                } else {
+                    vals[findi] = propchg.getVal();
+                }
+            }
+        }
+    }
+}
--- src/vm/manager/ClientTest.java
+++ src/vm/manager/ClientTest.java
@@ -1,6 +1,7 @@
 package vm.manager;
 
-import vm.helper.ClientSession;
+import vm.helper.VCClientSession;
+import vm.helper.VCCloneVM;
 
 /*
  * Created by huxia on 2017/3/13.
@@ -9,12 +10,13 @@ public class ClientTest {
     public static void main(String[] args) {
         // 连接vcenter与中断连接的测试代码
         try{
-            ClientSession.Connect();
+            VCClientSession.Connect();
+            VCCloneVM.run("Datacenter", "Datacenter/vm/Temptest", "CloneTest");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                ClientSession.Disconnect();
+                VCClientSession.Disconnect();
             }catch (Exception e) {
                 e.printStackTrace();
             }
